2a8e2dbfa8ba6f93a50faa6eba49e56145053d70
Brian Abbott <hi@brianabbott.com>
2025-08-13 11:05:17 -0400

    add HelloWorld.java

---
+++ 1-week-1/1-wed/HelloWorld.java
@@ -0,0 +1,16 @@
+/**
+ * File: HelloWorld.java
+ * Class: CSCI 1301
+ * Author: Christopher Williams
+ * Created on: Jun 5, 2017
+ * Last Modified: Aug 18, 2019
+ * Description: Display a String value to the console
+ */
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+}
