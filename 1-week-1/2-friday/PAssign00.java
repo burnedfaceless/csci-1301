9f91feef65b48d3179273efe7cc104484387840a
Brian Abbott <hi@brianabbott.com>
2025-08-16 11:31:02 -0400

    completed lab 1

---
+++ 1-week-1/2-friday/PAssign00.java
@@ -0,0 +1,18 @@
+/**
+ * File: PAssign00.java
+ * Class: CSCI 1301
+ * Author: Brian Abbott
+ * Created On: August 15, 2025
+ * Last Modified: August 15, 2025
+ * Description: Display Three Messages To The Console
+ */
+
+class PAssign00 {
+
+    public static void main(String[] args) {
+        System.out.println("This is Programming Assignment 0 for CSCI 1301.");
+        System.out.println("I have written my first \"official program\" and it works!");
+        System.out.println("Outputting text is not that hard, and I should make sure I get good at it.");
+    }
+
+}
