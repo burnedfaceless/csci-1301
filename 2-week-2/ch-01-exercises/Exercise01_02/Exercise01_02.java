55f2f0ffd0a505d0673ae625285b42826efa415c
Brian Abbott <hi@brianabbott.com>
2025-08-21 05:56:58 -0400

    add Exercise01_02.java

---
+++ 2-week-2/ch-01-exercises/Exercise01_02/Exercise01_02.java
@@ -0,0 +1,16 @@
+/* Paste your Exercise01_02 here and click Automatic Check.
+For all programming projects, the numbers should be double
+unless it is explicitly stated as integer.
+If you get a java.util.InputMismatchException error, check if
+your code used input.nextInt(), but it should be input.nextDouble().
+For integers, use int unless it is explicitly stated as long. */
+
+public class Exercise01_02 {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Welcome to Java");
+        }
+    }
+
+}
