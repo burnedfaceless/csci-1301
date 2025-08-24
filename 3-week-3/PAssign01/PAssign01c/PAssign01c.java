cf0fa3cc5e66c66bdbe2fc7e167788eeb1232499
Brian Abbott <hi@brianabbott.com>
2025-08-24 17:00:28 -0400

    finish PAssign01c.java

--- 3-week-3/PAssign01/PAssign01c/PAssign01c.java
+++ 3-week-3/PAssign01/PAssign01c/PAssign01c.java
@@ -9,13 +9,22 @@
 
 import java.util.Scanner;
 
-class PAssign01c {
+public class PAssign01c {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        // declare values to constant
-        final
+        // declare and set values
+        double volume = 0.25;
+        int numberOfMoles = 1;
+        final double R = 8.31446;
+        int temperature = 313;
+
+        // calculate pressure
+        double pressure = ((numberOfMoles * R * temperature) / volume);
+
+        // output to user
+        System.out.println("The pressure of " + numberOfMoles + " mole(s) of an ideal gas with volume " + volume + " m^3 at temperature " + temperature + " K is " + pressure + " pascals.");
     }
 
 }
