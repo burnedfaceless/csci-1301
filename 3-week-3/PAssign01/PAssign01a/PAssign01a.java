78788e0019bb7e63e72d5e5e6e24b89f8bff2ac9
Brian Abbott <hi@brianabbott.com>
2025-08-24 14:37:48 -0400

    add PAssign01a.java

---
+++ 3-week-3/PAssign01/PAssign01a/PAssign01a.java
@@ -0,0 +1,36 @@
+/**
+ * File: PAssign01a.java
+ * Class: CSCI 1301
+ * Author: Brian Abbott
+ * Created on: Aug 24, 2025
+ * Last Modified: Aug 24, 2025
+ * Description: Prompt user for number of beverages ($0.50) and sandwiches ($2.75) they are purchasing. Calculate total.
+ */
+
+import java.util.Scanner;
+
+class PAssign01a {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        // store cost of items in a constant
+        final double beveragePrice = 0.50;
+        final double sandwichPrice = 2.75;
+
+        System.out.print("Enter the number of Beverages: ");
+        double numOfBeverages = input.nextDouble();
+        System.out.print("Enter the number of sandwiches: ");
+        double numofSandwiches = input.nextDouble();
+
+        // calculate total cost
+        double totalCost = (numOfBeverages * beveragePrice) + (numofSandwiches * sandwichPrice);
+
+        System.out.println(totalCost);
+
+        // display output to user
+        System.out.println("The total of " + numOfBeverages + " beverages and " + numofSandwiches + " sandwiches is $" + totalCost + ".");
+    }
+
+}
