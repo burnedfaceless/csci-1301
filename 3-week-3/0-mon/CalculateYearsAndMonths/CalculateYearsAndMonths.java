900231be55a37cd149576a57766ea5dde3b24670
Brian Abbott <hi@brianabbott.com>
2025-08-25 10:44:41 -0400

    add CalculateYearsAndMonths.java

---
+++ 3-week-3/0-mon/CalculateYearsAndMonths/CalculateYearsAndMonths.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+class CalculateYearsAndMonths {
+
+    public static void main(String[] args) {
+        // input
+        Scanner input = new Scanner(System.in);
+        // prompt user for number of months
+        System.out.print("Enter total number of months: ");
+        int totalMonths = input.nextInt();
+
+        // processing
+        int totalYears = totalMonths / 12;
+        int totalMonthsLeftover = totalMonths % 12;
+
+        // output
+        System.out.println("You have " + totalYears + " year(s) and " + totalMonthsLeftover + " month(s) leftover.");
+
+
+    }
+
+}
